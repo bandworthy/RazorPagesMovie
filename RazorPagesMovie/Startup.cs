28a32e4bcaea06ee8da41da0d082b37e95e1171c
Shane Paul Drobnick <sdrobnick@internode.on.net>
2018-03-01 16:41:35 +1000

    end of day commit 1/3

--- RazorPagesMovie/Startup.cs
+++ RazorPagesMovie/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -21,6 +22,11 @@ namespace RazorPagesMovie
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // requires 
+            // using RazorPagesMovie.Models;
+            // using Microsoft.EntityFrameWorkCore;
+            services.AddDbContext<Models.MovieContext>(options =>
+               options.UseSqlServer(Configuration.GetConnectionString("MovieContext")));
             services.AddMvc();
         }
 
